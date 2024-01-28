004766672452003bf5a48709e11605e50f2ffb89
scottydixon <scotty.dixon@yahoo.com>
2024-01-27 18:50:47 -0600

    "moment js stuff"

--- src/DisplayArticles.jsx
+++ src/DisplayArticles.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
-
-
+import React from "react";
+import moment from "moment";
 
 function DisplayArticleCard(props) {
-
-  const commentURL ='https://news.ycombinator.com/item?id='+props.storyID
-  const authorURL = 'https://news.ycombinator.com/user?id='+props.author
-  const removez= props.created.replace("Z","")
-  const removetime=removez.substr(0,10)
-  const formattedDate = removetime.replace("T"," ")
+  const commentURL = "https://news.ycombinator.com/item?id=" + props.storyID;
+  const authorURL = "https://news.ycombinator.com/user?id=" + props.author;
+  const removez = props.created.replace("Z", "");
+  const removetime = removez.substr(0, 10);
+  const formattedDate = removetime.replace("T", " ");
+  const timeAgo = moment(props.created).fromNow();
 
   return (
-
-    <li  className="articles"> 
-        <a href={props.url} className="title">{props.title}</a>
-        <a href={props.url} target="_blank" class = "titleURL"> ( {props.url} )</a>
-        <br></br>
-        <a href={commentURL} target="_self" class="info">{props.points} points | </a>
-        <a href={authorURL} target="_self" class="info"> {props.author} | </a>
-        <a href={commentURL} target="_self" class="info"> {formattedDate} | </a>
-        <a href={commentURL} target="_self" class="info"> {props.comments} comments</a>
-  </li>
+    <li className="articles">
+      <a href={props.url} className="title">
+        {props.title}
+      </a>
+      <a href={props.url} target="_blank" class="titleURL">
+        {" "}
+        ( {props.url} )
+      </a>
+      <br></br>
+      <a href={commentURL} target="_self" class="info">
+        {props.points} points |{" "}
+      </a>
+      <a href={authorURL} target="_self" class="info">
+        {" "}
+        {props.author} |{" "}
+      </a>
+      <a href={commentURL} target="_self" class="info">
+        {" "}
+        {formattedDate} |{" "}
+      </a>
+      <a href={commentURL} target="_self" class="info">
+        {" "}
+        {timeAgo} |{" "}
+      </a>
+      <a href={commentURL} target="_self" class="info">
+        {" "}
+        {props.comments} comments
+      </a>
+    </li>
   );
 }
 
